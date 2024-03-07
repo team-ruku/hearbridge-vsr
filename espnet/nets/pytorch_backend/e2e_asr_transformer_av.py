d6f8c82ed2b48af9376c31dfc95df05168d70ed8
Hyunwoo Cho <truetype24@gmail.com>
2024-03-07 11:43:03 +0900

    chore: fix espnet errors

--- espnet/nets/pytorch_backend/e2e_asr_transformer_av.py
+++ espnet/nets/pytorch_backend/e2e_asr_transformer_av.py
@@ -18,10 +18,8 @@ from espnet.nets.pytorch_backend.nets_utils import (MLPHead, get_subsample,
                                                     make_non_pad_mask,
                                                     th_accuracy)
 from espnet.nets.pytorch_backend.transformer.add_sos_eos import add_sos_eos
-from espnet.nets.pytorch_backend.transformer.attention import \
-    MultiHeadedAttention  # noqa: H301
-from espnet.nets.pytorch_backend.transformer.attention import \
-    RelPositionMultiHeadedAttention  # noqa: H301
+from espnet.nets.pytorch_backend.transformer.attention import (  # noqa: H301
+    MultiHeadedAttention, RelPositionMultiHeadedAttention)
 from espnet.nets.pytorch_backend.transformer.decoder import Decoder
 from espnet.nets.pytorch_backend.transformer.encoder import Encoder
 from espnet.nets.pytorch_backend.transformer.label_smoothing_loss import \
@@ -203,6 +201,8 @@ class E2E(torch.nn.Module):
     @property
     def attention_plot_class(self):
         """Return PlotAttentionReport."""
+        from espnet.asr.asr_utils import PlotAttentionReport
+
         return PlotAttentionReport
 
     def __init__(self, odim, args, ignore_id=-1):
--- espnet/nets/pytorch_backend/transformer/encoder.py
+++ espnet/nets/pytorch_backend/transformer/encoder.py
@@ -12,20 +12,13 @@ from espnet.nets.pytorch_backend.backbones.conv1d_extractor import Conv1dResNet
 from espnet.nets.pytorch_backend.backbones.conv3d_extractor import Conv3dResNet
 from espnet.nets.pytorch_backend.nets_utils import rename_state_dict
 # from espnet.nets.pytorch_backend.transducer.vgg import VGG2L
-from espnet.nets.pytorch_backend.transformer.attention import \
-    LegacyRelPositionMultiHeadedAttention  # noqa: H301
-from espnet.nets.pytorch_backend.transformer.attention import \
-    MultiHeadedAttention  # noqa: H301
-from espnet.nets.pytorch_backend.transformer.attention import \
-    RelPositionMultiHeadedAttention  # noqa: H301
+from espnet.nets.pytorch_backend.transformer.attention import (  # noqa: H301
+    LegacyRelPositionMultiHeadedAttention, MultiHeadedAttention,
+    RelPositionMultiHeadedAttention)
 from espnet.nets.pytorch_backend.transformer.convolution import \
     ConvolutionModule
-from espnet.nets.pytorch_backend.transformer.embedding import \
-    LegacyRelPositionalEncoding  # noqa: H301
-from espnet.nets.pytorch_backend.transformer.embedding import \
-    PositionalEncoding  # noqa: H301
-from espnet.nets.pytorch_backend.transformer.embedding import \
-    RelPositionalEncoding  # noqa: H301
+from espnet.nets.pytorch_backend.transformer.embedding import (  # noqa: H301
+    LegacyRelPositionalEncoding, PositionalEncoding, RelPositionalEncoding)
 from espnet.nets.pytorch_backend.transformer.encoder_layer import EncoderLayer
 from espnet.nets.pytorch_backend.transformer.layer_norm import LayerNorm
 from espnet.nets.pytorch_backend.transformer.multi_layer_conv import (
