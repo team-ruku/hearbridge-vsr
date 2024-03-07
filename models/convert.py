add186a71daae5ee4f5602084917922911b063d6
Hyunwoo Cho <truetype24@gmail.com>
2024-03-07 20:40:29 +0900

    chore: remove unnecessary things

--- models/convert.py
+++
@@ -1,9 +0,0 @@
-import torch
-from unet import UNet
-
-model = UNet(3, 2)
-model.load_state_dict(torch.load("best_weights.pth"))
-model.eval()
-example = torch.rand(1, 3, 320, 480)
-traced_script_module = torch.jit.trace(model, example)
-traced_script_module.save("model.pt")
