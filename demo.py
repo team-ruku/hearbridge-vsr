c0531119e7e4b16b37233a8e6986d11c12a40ea1
Hyunwoo Cho <truetype24@gmail.com>
2024-04-17 22:32:47 +0900

    chore: time

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+import time
+
 import hydra
 from loguru import logger
 
@@ -6,10 +8,13 @@ from pipelines import InferencePipeline
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="hydra")
 def main(cfg):
+    start = time.time()
     logger.debug(f"Hydra config: {cfg}")
     pipeline = InferencePipeline(cfg)
     transcript = pipeline(cfg.filename)
+    end = time.time()
     print(f"transcript: {transcript}")
+    logger.debug(f"Exec time: {end-start}")
 
 
 if __name__ == "__main__":
